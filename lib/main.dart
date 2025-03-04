6bd664cbdc5c61dc7f9201c8bca6b9af6e46a885
Ketan satani <ketan.satani@theonetechnologies.co.in>
2025-03-04 19:48:56 +0530

    - Code commenting and refactorization made in the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,3 @@
+/// @nodoc
 library theone_book_designer;
-
 export 'theone_book_designer.dart';
--- lib/theone_book_designer.dart
+++ lib/theone_book_designer.dart
@@ -1,18 +1,26 @@
+/// The One Book Designer: A tool for designing books with customizable templates.
+///
+/// This library provides functionality to create, edit, and export book designs.
+library;
+
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'dart:collection';
 
 class TheOneBookDesigner extends StatelessWidget {
+  /// This will ask you to provide the web images you want show in the book view
   final List<String> imageUrls;
 
   const TheOneBookDesigner({super.key, required this.imageUrls});
 
   @override
   Widget build(BuildContext context) {
-    return loadWebView();
+    /// This will load the webview and load your images
+    return loadBookView();
   }
 
-  Widget loadWebView() {
+  /// This widget loads the images and load the core logic
+  Widget loadBookView() {
     InAppWebViewController? webView;
     InAppWebViewController.clearAllCache();
     final key = UniqueKey();
@@ -22,15 +30,14 @@ class TheOneBookDesigner extends StatelessWidget {
         initialFile: 'packages/theone_book_designer/assets/index.html',
         initialUserScripts: UnmodifiableListView<UserScript>([]),
         key: key,
-        initialOptions: InAppWebViewGroupOptions(
-          crossPlatform: InAppWebViewOptions(
-            supportZoom: false,
-            useShouldOverrideUrlLoading: true,
-            allowFileAccessFromFileURLs: true,
-            transparentBackground: true, // Enable transparency
-          ),
-          ios: IOSInAppWebViewOptions(),
-          android: AndroidInAppWebViewOptions(),
+        initialSettings: InAppWebViewSettings(
+          allowsInlineMediaPlayback: true,
+          transparentBackground: true,
+          disableVerticalScroll: true,
+          disableHorizontalScroll: true,
+          supportZoom: false,
+          useShouldOverrideUrlLoading: true,
+          allowFileAccessFromFileURLs: true,
         ),
         onWebViewCreated: (InAppWebViewController inAppController) {
           webView = inAppController;
@@ -39,12 +46,11 @@ class TheOneBookDesigner extends StatelessWidget {
           // Create a string representation of the image URLs
           final String imageArray = imageUrls.map((url) => "'$url'").join(',');
           await webView?.evaluateJavascript(
-            source:
-                "console.log('Adding pages:', [$imageArray]); addPages([$imageArray]);",
+            source: "console.log('Adding pages:', [$imageArray]); addPages([$imageArray]);",
           );
         },
-        onLoadError: (controller, url, code, message) {
-          debugPrint('Webview Error: $message');
+        onReceivedError: (controller, request, error) {
+          debugPrint('Webview Error: $error');
         },
       );
     } catch (e, st) {
